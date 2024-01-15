83e7bff0098525ffb4986ae922781c1e1e0f8382
Ramgami11050 <154421131+Ramgami11050@users.noreply.github.com>
2024-01-15 13:34:28 +0100

    Add files via upload

---
+++ Bruh.js
@@ -0,0 +1,16 @@
+function log() {
+    console.log('Clicked');
+  };
+  let frutas = ["Manzana", "Banana"];
+  console.log(frutas); // Manzana y Banana
+  console.log(frutas.length); // 2
+  let primero = frutas[0]; // Manzana
+  let ultimo = frutas[frutas.length - 1]; // Banana
+  let nuevaLongitud = frutas.push("Naranja"); // Añade "Naranja" al final
+  // ["Manzana", "Banana", "Naranja"]
+  ultimo = frutas.pop(); // Elimina "Naranja" del final
+  // ["Manzana", "Banana"]
+
+ console.log(); let nuevaLongitud2 = frutas.push("Kiwi");
+
+ 
---
+++ script.js
@@ -0,0 +1,3 @@
+function Bruh(){
+alert("LOL");
+}
