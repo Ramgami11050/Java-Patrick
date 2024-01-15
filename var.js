0f66bc2816628b4b79ff915b6817fdbb46435c54
Ramgami11050 <154421131+Ramgami11050@users.noreply.github.com>
2024-01-15 13:54:14 +0100

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+//this string is used to log (mayor y menor) operations onto the console...
+// ...upon clicking the button
+
+function log() {
+    console.log('Clicked');
+ 
+
+ var a = 9;
+ var b = 8;
+ console.log(a > b); //Devuelve true porque a es mayor que b
+ var c = 9;
+ console.log(a >= c); //Devuelve true porque a es igual que c
+
+ const cadena = "Hola";
+ const cadena2 = "Adios";
+ console.log(cadena > cadena2); //Devuleve true porque "Hola" es mayor alfabéticamente hablando
+ console.log(cadena < cadena2);
+};
