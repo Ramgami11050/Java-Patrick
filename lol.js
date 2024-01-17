8316f633f45509249f5e0b2c4f18e55287eb2d2f
Ramgami11050 <154421131+Ramgami11050@users.noreply.github.com>
2024-01-17 13:56:22 +0100

    Add files via upload

---
+++ lol.js
@@ -0,0 +1,30 @@
+var numero1
+var numero2
+var numero3
+var numero4
+var n
+
+function holaMundo() {
+  if (holaMundo)   
+    numero3=(numero1+numero2);
+    if (numero3)
+    alert("El resultado es"+ numero3)
+    //alert(num3);
+
+}
+  // parseint (conversion de tipos simple)
+  function holaMundo2() {
+    if ( holaMundo2) 
+    numero4=(numero1-numero2);
+    if (numero4)
+    alert("El resultado es"+ numero4)
+    //alert(num4);
+
+}
+  //console.log(holaMundo());
+  //console.log(holaMundo2());
+
+numero1=(prompt("Input un numero real"));
+numero2=(prompt("Input un numero real"));
+
+// SEARCH PARSEINT INFORMATION ON GOOGLE
