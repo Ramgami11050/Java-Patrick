5e01807a7c719c05628ac1f0efc9c1214da617ab
Ramgami11050 <154421131+Ramgami11050@users.noreply.github.com>
2024-01-17 21:57:00 +0100

    Add files via upload
    reference sheet

---
+++ RefSheet.js
@@ -0,0 +1,40 @@
+//VARIABLES (var)
+//var or let can be used to name different variables which will be used in the future.
+//STRING (TYPE OF VALUE): a sequence of characters (usually a name to determine a variables value)
+// Example:
+var a = "Mosh"
+var b
+var c 
+
+// Cannot be reserved
+// Cannot start with a number (1patrick)
+// Cannot contain a space or a hyphen (-)
+// Should be meaningful (not a, b, a1, etc...)
+// Are case sensitive (a variable named 'A' is different from 'a'
+
+//CONSTANTS (const)
+//the value of a variable CAN change. The value of a constant however, CAN't change.
+//If you don't need to reassign a variable, const is the best choice. Otherwise, if you need to reassign a variable, use var or let.
+
+//̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅
+// [  𝟭 & 𝟮   ]   𝗖𝗔𝗧𝗘𝗚𝗢𝗥𝗜𝗘𝗦 𝗢𝗙 𝗧𝗬𝗣𝗘𝗦
+// _________________________________
+
+//̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅
+// [    1    ] Primitives/Value Types
+//__________________________________
+// [ 1 ] String
+var name = "string";
+// [ 1 ] Number
+let age = 30;
+// [ 1 ] Boolean
+let isApproved = false;
+// [ 1 ] Undefined = Null. his is used in situations when you want to clear the value of a variable.
+let firstName = undefined; 
+// [ 1 ] Null = Undefined. This is used in situations when you want to clear the value of a variable.
+let lastName = null;
+
+//̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅
+// [    2    ] Reference Types
+//__________________________________
+// [ 2 ] Object
